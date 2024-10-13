b4863670f051362dedfcce612c91c01a87a81999
Trishir24 <168069165+Trishir24@users.noreply.github.com>
2024-10-13 11:26:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
-    alert("Thank you! Your message has been sent.");
-    
-    // Clear the form
-    this.reset();
-});
